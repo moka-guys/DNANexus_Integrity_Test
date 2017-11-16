8ee66123b20ad7cefaa52cf5926ef800895fa731
Andy Bond <andybond87@gmail.com>
2017-11-16 17:17:32 +0000

    Added comments to config file. See #1

--- config.sh
+++ config.sh
@@ -1,4 +1,8 @@
+# Path to the dx-toolkit environment
 DX_ENV=/etc/profile.d/dnanexus.environment.sh
+# DNANexus project that should be used for the run
 PROJECT=003_DNANexus_Integrity_Check
+# Path to the DNANexus workflow to run (assumed to be in above project)
 WORKFLOW=Workflows/GATK3.5_DNANexus_Integrity_Test
+# Path to the truth set output folders (assumed to be in above project)
 TRUTH_SET=Truth_Set
