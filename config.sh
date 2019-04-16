86a6438ea3047fb2b0df1ff14aefa396fdf4a536
Andy Bond <andybond87@gmail.com>
2019-04-16 12:14:43 +0100

    Use project ID rather than name

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # Path to the dx-toolkit environment
 DX_ENV=/etc/profile.d/dnanexus.environment.sh
 # DNANexus project that should be used for the run
-PROJECT=003_DNANexus_Integrity_Check
+PROJECT=project-F85Pb600gBbb98vQ0fgFKb1P
 # Path to the DNANexus workflow to run (assumed to be in above project)
 WORKFLOW=Workflows/GATK3.5_DNANexus_Integrity_Test
 # Path to the truth set output folders (assumed to be in above project)
