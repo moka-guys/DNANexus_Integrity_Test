f7b5bccbd3b23c5df6055493a2662e78e0f34e3c
Andy Bond <andybond87@gmail.com>
2019-05-02 15:16:45 +0100

    Move workflow to 001

--- config.sh
+++ config.sh
@@ -5,6 +5,6 @@ DX_ENV=/etc/profile.d/dnanexus.environment.sh
 # DNANexus project that should be used for the run
 PROJECT=project-FY4ZY8Q0ZZYX7QpYP2Fpk640
 # Workflow ID
-WORKFLOW=project-FY4ZY8Q0ZZYX7QpYP2Fpk640:workflow-FY4ZYk80ZZYf29pzBP5KgVZf
+WORKFLOW=project-ByfFPz00jy1fk6PjpZ95F27J:workflow-FY5PY1Q0jy1Z6P135ZFbyFpY
 # Path to the truth set output folders (assumed to be in above project)
 TRUTH_SET=Truth_Set
